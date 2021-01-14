00158e5170ff943765f1ca32f3cbffd61ab4aa0c
nirmalkumar1996 <37735723+nirmalkumar1996@users.noreply.github.com>
2021-01-14 15:00:42 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdint.h>
+#include "sort.h"
+#include<assert.h>
+
+void assert_sorting(int32_t list[],uint32_t size){
+    int32_t pass;
+    for (pass = 0; pass < size-1;++pass){
+       // printf("%d\n",list[pass]);
+        assert(list[pass] <= list[pass+1]);
+    }
+}
+
+void test_sort(){
+    int32_t input[] = {23,18,98,76,62,5,31,63,54,86};
+   /* bubble_sort(input,10);
+    assert_sorting(input,10);
+    selection_sort(input, 10);
+    assert_sorting(input,10);
+    insertion_sort(input, 10);
+    assert_sorting(input,10);*/
+    quick_sort(input, 0, 9);
+    assert_sorting(input, 10);
+    //merge_sort(input, 0, 9);
+    //assert_sorting(input, 10);
+
+}
+
+int main()
+{
+    //printf("Hello world!\n");
+    test_sort();
+    return 0;
+}
